92510288057d0a7612fb4974ce3103b1c89d28b0
Your Name <you@example.com>
2022-07-25 13:45:37 -0700

    navbar

--- views/navbar.php
+++ views/navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav-wrapper blue">
-      <a href="#" class="brand-logo">Proyecto con Paypal</a>
+      <a href="#" class="brand-logo">Proyecto con Paypal TEAM perro</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a id="CerrarSesion" class="waves-effect waves-light btn blue-4">Cerrar Sesion</a></li>
       </ul>
